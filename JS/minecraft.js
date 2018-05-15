f03064a27613764bb6010a642121b1bd684280f9
johan <johan.elberg38@gmail.com>
2018-05-15 18:09:36 +0300

    displayed layout

--- JS/minecraft.js
+++ JS/minecraft.js
@@ -1,6 +1,5 @@
-var minecraftGame = {
+var minecraftGame = {};
 
-};
 
 minecraftGame.init = function () {
     minecraftGame.layout();
@@ -8,28 +7,77 @@ minecraftGame.init = function () {
 
 minecraftGame.layout = function () {
     var map = {
-        cols: 15,
-        rows: 15,
-        tSize: 225,
+        cols: 22,
+        rows: 22,
         tiles: [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 5, 0, 0, 0, 5, 5, 0, 5, 0, 0, 0, 0, 2, 0, 0, 5, 0, 0, 0],
+            [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ],
         getTile: function (col, row) {
             return this.tiles[col][row]
+        },
+    }
+
+    for (var c = 0; c < map.cols; c++) {
+        for (var r = 0; r < map.rows; r++) {
+            var tile = map.getTile(c,r);
+            if (tile == 0) {
+                var divEmpty = $("<div/>");
+                divEmpty.addClass('divEmpty');
+                divEmpty.addClass('col-1');
+                $('#bigBox').append(divEmpty);
+            } else if (tile == 1) {
+                var divGround = $("<div/>");
+                divGround.addClass('divGround');
+                divGround.addClass('col-1');
+                $('#bigBox').append(divGround);
+            } else if (tile == 2) {
+                var divWood = $("<div/>");
+                divWood.addClass('divWood');
+                divWood.addClass('col-1');
+                $('#bigBox').append(divWood);
+            } else if (tile == 3) {
+                var divLeaf = $("<div/>");
+                divLeaf.addClass('divLeaf');
+                divLeaf.addClass('col-1');
+                $('#bigBox').append(divLeaf);
+            } else if (tile == 4) {
+                var divCloud = $("<div/>");
+                divCloud.addClass('divCloud');
+                divCloud.addClass('col-1');
+                $('#bigBox').append(divCloud);
+            } else if (tile == 5) {
+                var divStone = $("<div/>");
+                divStone.addClass('divStone');
+                divStone.addClass('col-1');
+                $('#bigBox').append(divStone);
+            } else if (tile == 6) {
+                var divGrass = $("<div/>");
+                divGrass.addClass('divGrass');
+                divGrass.addClass('col-1');
+                $('#bigBox').append(divGrass);
+            }
         }
     }
-}
+
+};
+
+
+
+           
+
+
+minecraftGame.init();
